6092409dcca99f0ecd96dd1ba6e4a95b777afa97
SeanShin0821 <cshin4@gmu.edu>
2022-02-18 15:56:14 -0500

    question update

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+//Declaring var 
+
+
+
+
+
+
+
+//Function for buttons 
+var buttonA = document.getElementById("A");
+var buttonB = document.getElementById("B");
+var buttonC = document.getElementById("C");
+var buttonD = document.getElementById("D");
+
+
+//Quiz Questions and Answer 
+
+var quizQuestions = 
+[ {
+            question: "What does HTML stand for ?",
+            choiceA:  "Hyper Trainer Marking Language",
+            choiceB:  "Hyper Text Marketing Language",
+            choiceC:  " Hyper Text Markup Language",
+            choiceD:  "Hyper Text Markup Leveler",
+            correctAnswer: "C"},
+        {
+            question: "Which of the following is the correct way to use the standard namespace in C++?",
+            choiceA:  "Using namespace std; ",
+            choiceB:  "Using namespace standard; ",
+            choiceC:  "Using standard namespace; ",
+            choiceD:  "Standard namespace used; ",
+            correctAnswer: "A"},
+        {
+            question: "What does HTML stand for ?",
+            choiceA:  "Hyper Trainer Marking Language",
+            choiceB:  "Hyper Text Marketing Language",
+            choiceC:  " Hyper Text Markup Language",
+            choiceD:  "Hyper Text Markup Leveler",
+            correctAnswer: "C"},
+        {
+            question: "What does HTML stand for ?",
+            choiceA:  "Hyper Trainer Marking Language",
+            choiceB:  "Hyper Text Marketing Language",
+            choiceC:  " Hyper Text Markup Language",
+            choiceD:  "Hyper Text Markup Leveler",
+            correctAnswer: "C"},
+            {
+            question: "What does HTML stand for ?",
+            choiceA:  "Hyper Trainer Marking Language",
+            choiceB:  "Hyper Text Marketing Language",
+            choiceC:  " Hyper Text Markup Language",
+            choiceD:  "Hyper Text Markup Leveler",
+            correctAnswer: "C"},
+
+
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
