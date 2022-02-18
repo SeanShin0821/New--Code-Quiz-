8146a04a511ca928b7cc6f25d778b8b163bdd7dc
SeanShin0821 <cshin4@gmu.edu>
2022-02-18 16:24:07 -0500

    Question update

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ var buttonD = document.getElementById("D");
 
 
 //Quiz Questions and Answer 
-
 var quizQuestions = 
 [ {
             question: "What does HTML stand for ?",
@@ -31,34 +30,30 @@ var quizQuestions =
             choiceD:  "Standard namespace used; ",
             correctAnswer: "A"},
         {
-            question: "What does HTML stand for ?",
-            choiceA:  "Hyper Trainer Marking Language",
-            choiceB:  "Hyper Text Marketing Language",
-            choiceC:  " Hyper Text Markup Language",
-            choiceD:  "Hyper Text Markup Leveler",
+            question: "Who is making the Web standards ?",
+            choiceA:  "Google",
+            choiceB:  "Microsoft",
+            choiceC:  "The World Wide Web Consortium",
+            choiceD:  "Mozilla",
             correctAnswer: "C"},
         {
-            question: "What does HTML stand for ?",
-            choiceA:  "Hyper Trainer Marking Language",
-            choiceB:  "Hyper Text Marketing Language",
-            choiceC:  " Hyper Text Markup Language",
-            choiceD:  "Hyper Text Markup Leveler",
-            correctAnswer: "C"},
+            question: "How can you make a numbered list ?",
+            choiceA:  "<list>",
+            choiceB:  "<ol>",
+            choiceC:  "<ul>",
+            choiceD:  "<dl>",
+            correctAnswer: "B"},
             {
-            question: "What does HTML stand for ?",
-            choiceA:  "Hyper Trainer Marking Language",
-            choiceB:  "Hyper Text Marketing Language",
-            choiceC:  " Hyper Text Markup Language",
-            choiceD:  "Hyper Text Markup Leveler",
+            question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+            choiceA:  "src",
+            choiceB:  "longdesc",
+            choiceC:  "alt",
+            choiceD:  "title",
             correctAnswer: "C"},
 
+];
 
 
-
-
-
-
-]
 
 
 
